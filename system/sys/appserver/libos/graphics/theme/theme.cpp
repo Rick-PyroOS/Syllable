15c3ee881c3ee0b0c193b2c1377e1281aa5e3536
Rick Caudill <rick.g.caudill@gmail.com>
2025-10-04 21:51:33 -0600

    changes...

--- system/sys/appserver/libos/graphics/theme/theme.cpp
+++ system/sys/appserver/libos/graphics/theme/theme.cpp
@@ -3,28 +3,40 @@
 
 class Theme::Private{
 public:
-    graphics::theme::style::Style* m_cDarkStyle;
-    graphics::theme::style::Style* m_cLightStyle;
+    os::String m_cFile;
+    graphics::theme::style::Style m_cStyle;
+    ThemeInfo m_cInfo;
 }
 
 Theme::Theme() {
     m = new Private;
-    m->m_cDarkStyle = NULL;
-    m->m_cLightStyle = NULL;
-}
-
-Theme::Theme(const os::File& cFile){
-    assert(new os::FSNode(cFile).IsValid() == false);
-    __ReadFromFile__(cFile);
+    m->m_cStyle = NULL;
 }
 
 Theme::Theme(const os::String& cFile){
     assert(new os::FSNode(cFile).IsValid() == false);
-    __ReadFromFile__(os::File(cFle));
+    m->m_cFile = cFile;
+    m->m_cStyle = new graphics::theme::style::Style();
+    m->m_cInfo = ThemeInfo();
+    __ReadFromFile__(cFile);
 }
 
+graphics::theme::style::Style Theme::GetStyle() const{
+    return m->m_cStyle;
+}
 
-void Theme::__ReadFromFile__(const os::File&){
-    m->m_cDarkStyle = new graphics::theme::Style();
-    m->m_cLightStyle = new graphics::theme::Style();
+ThemeInfo Theme::GetInfo() const{
+    return m->m_cInfo;
+}
+
+void Theme::__ReadFromFile(const os::File& file){
+    Settings* settings = new os::Settings(file);
+    m->m_cInfo.m_cAuthor = settings->GetString("author","");
+    m->m_cInfo.m_cDescription = settings->GetString("description","");
+    m->m_cInfo.m_cImageFile = settings->GetString("image","");
+    m->m_cInfo.m_cStyleFile = settings->GetString("style","");
+    assert(new os::FSNode(m->m_cInfo.m_cImageFile).IsValid() == false);
+    assert(new os::FSNode(m->m_cInfo.m_cStyleFile).IsValid() == false);
+
+
 }
--- system/sys/include/pyro/pyro/graphics/theme/theme.h
+++ system/sys/include/pyro/pyro/graphics/theme/theme.h
@@ -7,29 +7,29 @@
 #include <storage/file.h>
 namespace graphics {
     namespace theme {
+        struct ThemeInfo {
+            os::String m_cAuthor;
+            os::String m_cDescription;
+            os::String m_cStyleFile;
+            os::String m_cImageFile;
+        };
         class Theme
         {
             public:
                 Theme();
-                Theme(const os::File&);
-
-                graphics::theme::style::Style GetDarkMode() const{
-                    return m->m_cDarkStyle;
-                }
-                graphics::theme::style::Style GetLightMode() const{
-                    return m->m_cLightStyle;
-                }
+                graphics::theme::style::Style GetStyle() const;
+                ThemeInfo GetThemeInfo() const;
             private:
                 class Private;
                 Private *m;
 
-                void __ReadFromFile__(const os::File&);
+                void __ReadFromFile(const os::String& cFile);
                 void __Init__();
         };
     }
 }
 
-const graphics::theme::Theme DEFAULT_THEME = graphics::theme::Theme(os::File("/system/themes/.default/style.css"));
+const graphics::theme::Theme DEFAULT_THEME = graphics::theme::Theme(os::File("/system/themes/default/style.css"));
 
 
 #endif
